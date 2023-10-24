13054ae97e9c4ed3f03696452f96385d300899b6
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-10-23 21:15:57 -0600

    Exercise complete

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const {
+    getHouses,
+    deleteHouse, 
+    createHouse, 
+    updateHouse
+} = require('./controller')
+
+app.get(`/api/houses`, getHouses)
+app.delete(`/api/houses/:id`, deleteHouse)
+app.post(`/api/houses`, createHouse)
+app.put(`/api/houses/:id`, updateHouse)
+
+const port = 4004
+
+app.listen(4004, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
